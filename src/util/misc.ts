049da3b477d64187a62da7fa1ca43598f23f1082
Niedziolka Michal <niedziolek@hotmail.com>
2023-04-19 23:37:16 +0200

    Improve typing of sort func

--- src/util/misc.ts
+++ src/util/misc.ts
@@ -2,6 +2,6 @@ export function arrayFrom<T>(length: number, map: (index: number) => T) {
   return Array.from({ length }, (_, index) => map(index));
 }
 
-export function sort<T>(array: T[], key: keyof T): T[] {
-  return array.sort((e1, e2) => +e1[key] - +e2[key]);
+export function sort<T extends Record<K, number | bigint>, K extends keyof T>(array: T[], key: K): T[] {
+  return array.sort((e1, e2) => e1[key] - e2[key]);
 }
